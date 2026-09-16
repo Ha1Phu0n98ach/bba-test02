21744cca75d8e697681e14fbd09c710413627b7d
test 02 - Phuong Bach Hai <haiphuong8203@gmail.com>
2026-09-16 22:29:19 +0700

    feat: add solution for calm belt challenge

---
+++ tests/lesson-04/calm-belt.js
@@ -0,0 +1,12 @@
+let navigationName = "Luffy";
+let currentHakiLevel = 3;
+const ISLAND_A = 15;
+const ISLAND_B = 25;
+const ISLAND_C = 40;
+
+let sum = ISLAND_A + ISLAND_B + ISLAND_C;
+let trungBinhCong = sum / 3;
+let phanDu = sum % 3;
+console.log(sum);
+console.log(trungBinhCong);
+console.log(phanDu);
